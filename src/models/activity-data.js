6bca22d59910959000e6aa540c08fdb77e85c4e3
dvmarinoff <dvmarinoff@gmail.com>
2021-10-25 18:37:13 +0300

    adding ActivityData
    - this will know how to store data coming from a source and prepare it for FITjs

---
+++ src/models/activity-data.js
@@ -0,0 +1,169 @@
+import { existance  } from '../functions.js';
+import { fitjs } from '../fit/fitjs.js';
+import { fit } from '../fit/fit.js';
+import { dateToDashString } from '../utils.js';
+
+function Record(args = {}) {
+    const defaults = {
+        timestamp: (_ => Date.now()),
+        power:     0,
+        cadence:   0,
+        speed:     0,
+        heartRate: 0,
+        distance:  0,
+    };
+
+    const timestamp = existance(args.timestamp, defaults.timestamp());
+    const power     = existance(args.power, defaults.power);
+    const cadence   = existance(args.cadence, defaults.cadence);
+    const speed     = existance(args.speed, defaults.speed);
+    const heartRate = existance(args.heartRate, defaults.heartRate);
+    const distance  = existance(args.distance, defaults.distance);
+
+    return {
+        timestamp,
+        power,
+        cadence,
+        speed,
+        heartRate,
+        distance,
+    };
+}
+
+function Lap(args = {}) {
+    const defaults = {
+        timestamp:  (_ => Date.now()),
+        start_time: 0,
+    };
+
+    const timestamp          = existance(args.timestamp, defaults.timestamp());
+    const start_time         = existance(args.start_time, defaults.start_time);
+    const total_elapsed_time = (timestamp - start_time);
+
+    return {
+        timestamp,
+        start_time,
+        total_elapsed_time: total_elapsed_time,
+    };
+}
+
+function Event(args = {}) {
+    const defaults = {
+        timestamp:   (_ => Date.now()),
+        event:       0,
+        event_type:  0,
+        event_group: 0,
+    };
+
+    const timestamp   = existance(args.timestamp, defaults.timestamp());
+    const event       = existance(args.event, defaults.event);
+    const event_type  = existance(args.event_type, defaults.event_type);
+    const event_group = existance(args.event_group, defaults.event_group);
+
+    return {
+        timestamp,
+        event,
+        event_type,
+        event_group,
+    };
+}
+
+function TimerStart(args = {}) {
+    return Event({timestamp: args.timestamp, event: 0, event_type: 0});
+}
+
+function TimerPause(args = {}) {
+    return Event({timestamp: args.timestamp, event: 0, event_type: 1});
+}
+
+function TimerStop(args = {}) {
+    return Event({timestamp: args.timestamp,event: 0, event_type: 4});
+}
+
+function WorkoutStart(args = {}) {
+    return Event({timestamp: args.timestamp, event: 3, event_type: 0});
+}
+
+function WorkoutPause(args = {}) {
+    return Event({timestamp: args.timestamp, event: 3, event_type: 1});
+}
+
+function WorkoutStop(args = {}) {
+    return Event({timestamp: args.timestamp,event: 3, event_type: 4});
+}
+
+const Events = {
+    Event: Event,
+    TimerStart: TimerStart,
+    TimerPause: TimerPause,
+    TimerStop: TimerStop,
+    WorkoutStart: WorkoutStart,
+    WorkoutPause: WorkoutPause,
+    WorkoutStop: WorkoutStop,
+};
+
+function ActivityData(args = {}) {
+    const defaults = {
+        records: [],
+        laps:    [],
+        events:  [],
+    };
+
+    let records = existance(args.records, defaults.records);
+    let laps    = existance(args.laps, defaults.laps);
+    let events  = existance(args.events, defaults.events);
+
+    function getRecords() {
+        return records;
+    }
+
+    function getLaps() {
+        return laps;
+    }
+
+    function getEvents() {
+        return events;
+    }
+
+    function addRecord(record) {
+        records.push(record);
+    }
+
+    function addLap(lap) {
+        laps.push(lap);
+    }
+
+    function addEvent(event) {
+        events.push(event);
+    }
+
+    function activityFileName() {
+        const now = new Date();
+        return `workout-${dateToDashString(now)}.fit`;
+    }
+
+    function encode() {
+        const fitjsActivity    = fitjs.encode({records, laps, events});
+        const activity         = fit.activity.encode(fitjsActivity);
+        const activityFileName = activityFileName();
+
+        return {
+            activityFileName,
+            activity,
+        };
+    }
+
+    return Object.freeze({
+        getRecords,
+        getLaps,
+        getEvents,
+        addRecord,
+        addLap,
+        addEvent,
+
+        encode,
+        activityFileName,
+    });
+}
+
+export { ActivityData, Record, Lap, Events };
